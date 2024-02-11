d26ba0f07a0d671a036b93985084df5fcd19e912
Amir <herus2006cripto.gmail.com>
2024-02-11 14:31:34 +0330

    fix back-end bug

--- pages/api/purchaseApi.js
+++ pages/api/purchaseApi.js
@@ -11,27 +11,27 @@ export default async function hanlder(req , res){
     }
 
  /*---------------------- needed data --------------------------------- */
-//     const session = await getServerSession(req , res , authOptions);
-//     let selectedUser = await User.findOne({Email : session.user.email})
+    const session = await getServerSession(req , res , authOptions);
+    let selectedUser = await User.findOne({Email : session?.user.email})
 // /*---------------------- needed data --------------------------------- */
 
-//     if(req.method === 'PATCH'){
-//         let data = req.body;
-//         selectedUser.PurchaseList = data;
-//         await selectedUser.save();
-//         res.status(201).json({status : 'success', data : selectedUser.PurchaseList})
-//     }
+    if(req.method === 'PATCH'){
+        let data = req.body;
+        selectedUser.PurchaseList = data;
+        await selectedUser.save();
+        res.status(201).json({status : 'success', data : selectedUser.PurchaseList})
+    }
 
     if(req.method === 'GET'){
          res.status(200).json({ status : 'success' , data : []})
     }
 // selectedUser.PurchaseList
-    // if(req.method === 'DELETE'){
-    //     let {id , reduxState} = req.body;
-    //     let findItemForDelete = reduxState.filter(item => item.id != id)
-    //     selectedUser.PurchaseList = findItemForDelete;
-    //     await selectedUser.save();
-    //     res.status(200).json({status : 'success' , data : findItemForDelete})
-    // }
+    if(req.method === 'DELETE'){
+        let {id , reduxState} = req.body;
+        let findItemForDelete = reduxState.filter(item => item.id != id)
+        selectedUser.PurchaseList = findItemForDelete;
+        await selectedUser.save();
+        res.status(200).json({status : 'success' , data : findItemForDelete})
+    }
 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import HomePage from "@/component/template/HomePage";
 
 export default function Home({data}) {
-
   return <HomePage {...data}/>
 }
 
 export async function getStaticProps(){
+  // console.log(process.env.URI_DATA)
     let procces = await fetch(process.env.URI_DATA)
     let data = await procces.json()
     return {
